404fd1889f8a025c35f20ed5d41a70eee54182df
rocio <bere300801@gmail.com>
2023-03-04 22:28:10 -0600

    Se agrega el Proyecto

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <link rel="stylesheet" type="text/css" href="estilos.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>login</title>
+    
+</head>
+</body>
+    <form action="validar.php" method="post" style="text-align: center;" >
+    <font size="7" color="yellow"><h1> Bienvenido a la tienda RMJ</h1>
+    <font size="6"><p>Usuario <input type="text" placeholder="ingrese nombre de usuario" name="usuario"></p>
+    <p>Contraseña <input type="password" placeholder="ingrese su contraseña" name="password"></p></font></font>
+    <input type="submit" value="Ingresar">  
+    
+    </form>
+    <br>
+    <center><input type="submit" value="Resgistrate"></center>
+</body>
+</html>
